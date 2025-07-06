7a97673c11328bd8a8fbfa75e6ea923d5627814a
AlissonPolo <alissonjhojanatorres@gmail.com>
2025-07-06 12:52:24 -0500

    Cherry pick

--- app/src/main/java/com/example/proyectofinal/model/Contacto.kt
+++ app/src/main/java/com/example/proyectofinal/model/Contacto.kt
@@ -8,8 +8,16 @@ import java.util.Locale
 data class Contacto(
     val id: Int,
     val nombre: String,
-    val ultimoMensaje: String = "",
-    val horaUltimoMensaje: String = "",
+    var ultimoMensaje: String = "",
+    var horaUltimoMensaje: String = "",
     val fotoPerfilResId: Int = R.drawable.ic_person,
     val mensajes: MutableList<Mensaje> = mutableListOf()
-)
+){
+    fun agregarMensaje(mensaje: Mensaje) {
+        mensajes.add(mensaje)
+        ultimoMensaje = mensaje.texto
+        horaUltimoMensaje = SimpleDateFormat("hh:mm a", Locale.getDefault()).format(Date(mensaje.timestamp))
+    }
+
+    //prueba de cherry pick
+}
